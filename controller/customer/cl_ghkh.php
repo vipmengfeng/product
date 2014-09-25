77785ea5d93eb860ef1633da7c6f015f976beb40
sheyanyang <aa@cc.com>
2014-09-25 15:53:04 +0800

    DEL:w

--- controller/customer/cl_ghkh.php
+++ controller/customer/cl_ghkh.php
@@ -72,7 +72,7 @@
 				);
 				$query=$db->update(" where cusid in ($str)", "crm_customer" ,$data);
 				if($query){
-					echo "<script language=\"javascript\">window.location.reload('{$conf['log_out']}/controller/system/index.php?menuid=5');</script>";
+					echo "<script language=\"javascript\">javascript:location.href='{$conf['log_out']}/controller/system/index.php?menuid=5'</script>";
 				}else {
 					echo "<script language=\"javascript\">alert('获取失败')</script>";
 				}
@@ -83,7 +83,7 @@
 				);
 				$query=$db->update(" where cusid in ($str)", "crm_customer" ,$data);
 				if($query){
-					echo "<script language=\"javascript\">window.location.reload('{$conf['log_out']}/controller/system/index.php?menuid=5');</script>";
+					echo "<script language=\"javascript\">javascript:location.href='{$conf['log_out']}/controller/system/index.php?menuid=5'</script>";
 				}else {
 					echo "<script language=\"javascript\">alert('删除失败')</script>";
 				}			
--- controller/system/yhgl.php
+++ controller/system/yhgl.php
@@ -1,7 +1,7 @@
 <?php
 	global $conf;
 	$table_pre=$conf['pre'];
-	$sql="select * from ".$table_pre.'user';
+	$sql="select * from ".$table_pre.'user where disable=1';
 	$query=mysql_query($sql);
 	$res=array();
 		while(($row=mysql_fetch_assoc($query))==true){
