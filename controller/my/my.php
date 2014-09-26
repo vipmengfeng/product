7fa6d516ad7f98d0c3d067b0e15e5e47488ee59d
wenshichun <aa@bb.com>
2014-09-26 13:59:32 +0800

    del

--- controller/my/my.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	require '../../caches/caches_common/status.php';
-	$uid=$_SESSION['usernameid'];
-	$sql="select * from ".$table_pre."customer where disable=1 and uid=".$uid;
-	$r=$db->get_all($sql);
-	include template("wdkh","my");
-?>
