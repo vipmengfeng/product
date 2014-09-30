1deebd6224f9a48d91efbfc4a5712a687ad23cde
wenshichun <aa@bb.com>
2014-09-30 15:13:07 +0800

    杂项的修改

--- caches.php
+++ caches.php
@@ -2,11 +2,16 @@
 header("Content-type: text/html; charset=utf-8");
 	require "ini.php";
 	require "test.php";
-	$arr=get_list(0);
+	//$arr=get_list(0);
 	//print_r($arr);
-	
-	caches("menu",$arr,"caches/caches_common");
-	caches("status","","caches/caches_common");
-
+	   $sqlrole="SELECT * FROM crm_role";
+	    $re=$db->get_all($sqlrole);
+		foreach($re as $key=>$value){
+			$role[$value['roleid']]=$value;
+		
+		}
+	//caches("menu",$arr,"caches/caches_common");
+	//caches("status","","caches/caches_common");
+      caches("role",$role,"caches/caches_common");
 
 ?>
