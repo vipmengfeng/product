d17567bf00f4f9ddf9fe1872563d77ba6ff2b3ef
wenshichun <aa@bb.com>
2014-09-30 16:27:27 +0800

    缓存

--- caches.php
+++ caches.php
@@ -2,7 +2,7 @@
 header("Content-type: text/html; charset=utf-8");
 	require "ini.php";
 	require "test.php";
-	//$arr=get_list(0);
+	$arr=get_list(0);
 	//print_r($arr);
 	   $sqlrole="SELECT * FROM crm_role";
 	    $re=$db->get_all($sqlrole);
@@ -10,7 +10,7 @@ header("Content-type: text/html; charset=utf-8");
 			$role[$value['roleid']]=$value;
 		
 		}
-	//caches("menu",$arr,"caches/caches_common");
+	caches("menu",$arr,"caches/caches_common");
 	//caches("status","","caches/caches_common");
       caches("role",$role,"caches/caches_common");
 
