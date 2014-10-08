d6606e569172799c9072579ce9cfb1167b98bf31
sheyanyang <aa@cc.com>
2014-10-08 09:45:12 +0800

    通知管理完成

--- controller/system/sy.php
+++ controller/system/sy.php
@@ -3,8 +3,12 @@ require ROOT_DIR."/caches/caches_common/status.php";
 //查看新添加的客户
 	$sql="SELECT * FROM crm_customer ORDER BY inputtime DESC LIMIT 0,5";
 	$res=$db->get_all($sql);
-	$sql="SELECT * FROM crm_customer ORDER BY inputtime DESC LIMIT 5,10";
-	$re=$db->get_all($sql);
+	$sql1="SELECT * FROM crm_customer ORDER BY inputtime DESC LIMIT 5,10";
+	$re=$db->get_all($sql1);
+	
+	//查看通知
+	$sql2="SELECT * FROM crm_tz ORDER BY inputtime DESC LIMIT 0,5";
+	$tz=$db->get_all($sql2);
 	include template('index','system');
 	
 ?>
--- controller/system/tzgl.php
+++ controller/system/tzgl.php
@@ -9,11 +9,57 @@
 		include template("jump");
 		die;
 	}
-	if(empty($_GET)){
+if(empty($_GET)){
+		$sql="SELECT * FROM crm_tz WHERE disable=1";
+		$query=$db->get_all($sql);
 		include template('tzgl','system');
 	}
 	if($_GET['file'] == 'add'){
+		$id=$_GET['id'];
+		$action=$_GET['action'];
+		if(!empty($id)){
+			$sql="SELECT * FROM crm_tz WHERE id=$id";
+			$query=$db->get_one($sql);
+		}
 		include template('tzgl_add','system');
 	}
+	if($_GET['file'] == 'addtz'){
+			if($_GET['action'] == 'mod'){
+				$id=$_GET['id'];
+				$info=$_POST['info'];
+				if($db->update(" WHERE id=$id","crm_tz",$info)){
+					$url="{$conf['log_out']}/controller/system/tzgl.php";
+					$content="修改成功";
+					include template("jump");
+				}
+			}else{
+				$info=$_POST['info'];
+				$info['inputtime']=time();
+				if($db->add("crm_tz",$info)){
+					$url="{$conf['log_out']}/controller/system/tzgl.php";
+					$content="添加成功";
+					include template("jump");
+				}
+			}
+	}
 	
+	if($_GET['file'] == 'myform'){
+		if(!empty($_POST)){
+			$chec=$_POST['checkbox1'];
+			$str=implode(",", $chec);
+			$data=array(
+					"disable"=>"0",
+			);
+			$query=$db->update(" where id in ($str)", "crm_tz" ,$data);
+			if($query){
+				$url="{$conf['log_out']}/controller/system/tzgl.php";
+				$content="删除成功";
+				include template("jump");
+			}else {
+				$url="{$conf['log_out']}/controller/system/tzgl.php";
+				$content="删除失败";
+				include template("jump");
+			}
+		}
+	}
 ?>
