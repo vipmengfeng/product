5a20eeb29fe48b5a8898530d378c7df77362b365
wenshichun <aa@bb.com>
2014-10-14 16:42:32 +0800

    顶部未读消息

--- controller/left.php
+++ controller/left.php
@@ -9,5 +9,26 @@
 	$mes=$db->get_all($sql1);
 	$nums=$db->get_one($sql2);
 	$num=$nums['num'];
+	
+	$sql3="SELECT tz from crm_role WHERE roleid=' $_SESSION[mxrole]'";
+	$crole=$db->get_one($sql3);
+	$tz=substr($crole[tz],0,-1);
+	
+	$sql6="SELECT tz from crm_user WHERE id='$_SESSION[usernameid]'";
+	$readtz=$db->get_one($sql6);
+		$nread=explode(",",$tz);
+		$read=explode(",",substr($readtz['tz'],0,-1));
+		$weidu=array_diff($nread,$read);
+
+		 foreach($weidu as $c=>$d){
+			$tzs .=$d.",";
+		 }
+		$tzs=substr($tzs,0,-1);
+		
+		$sql4="SELECT * FROM crm_tz WHERE disable=1 AND id in ($tzs) ORDER BY inputtime DESC LIMIT 0,7";
+	$sql5="SELECT count(*) AS num FROM crm_tz WHERE disable=1 AND id in ($tzs) ORDER BY inputtime DESC ";
+	$number=$db->get_all($sql5);
+	$hh=$db->get_all($sql4);
+	$nums=$number[0]['num'];
 	require ROOT_DIR."/caches/caches_common/menu.php";
 ?>
--- controller/system/sy.php
+++ controller/system/sy.php
@@ -5,8 +5,11 @@ require ROOT_DIR."/caches/caches_common/status.php";
 	$res=$db->get_all($sql);
 	$sql1="SELECT * FROM crm_tx ORDER BY id DESC ";
 	$tx=$db->get_all($sql1);
-	$sql2="SELECT * FROM crm_tz ORDER BY inputtime DESC ";
-	$tz=$db->get_all($sql2);
+	$sql3="SELECT tz from crm_role WHERE roleid=' $_SESSION[mxrole]'";
+	$crole=$db->get_one($sql3);
+	$tz=substr($crole['tz'],0,-1);
+	$sql4="SELECT * FROM crm_tz WHERE disable=1 AND id in ($tz) ORDER BY inputtime DESC ";
+	$tz=$db->get_all($sql4);
 	include template('index','system');
 	
 ?>
